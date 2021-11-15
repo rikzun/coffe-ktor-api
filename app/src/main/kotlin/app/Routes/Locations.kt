d00df9b79a3a32b799324aa9c887d71d7b815f82
rikzun <rik.zunqq@gmail.com>
2021-11-15 10:47:52 +0300

    add new methods: create and delete for locations, also added some new locations in db

--- app/src/main/kotlin/app/Routes/Locations.kt
+++ app/src/main/kotlin/app/Routes/Locations.kt
@@ -7,6 +7,10 @@ import io.ktor.auth.*
 import io.ktor.http.*
 import io.ktor.response.*
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.valiktor.functions.isNotBlank
+import org.valiktor.functions.isNotEmpty
+import org.valiktor.functions.isNotNull
+import java.math.BigDecimal
 
 @Route(Method.GET, "/locations")
 suspend fun locationsList(call: ApplicationCall, principal: Principal) = call.respond(
@@ -26,4 +30,43 @@ suspend fun locationMenuList(call: ApplicationCall, principal: Principal) {
             .find { LocationMenuTable.locationID eq locationID.toInt() }
             .map { it.toRespondInfo() }
     })
+}
+
+data class LocationCreateParams(
+    val name: String,
+    val latitude: BigDecimal,
+    val longitude: BigDecimal
+)
+
+@Route(Method.POST, "/location/create")
+suspend fun locationCreate(call: ApplicationCall, principal: Principal) {
+    val params = call.receiveAndValid<LocationCreateParams> {
+        validate(LocationCreateParams::name).isNotEmpty().isNotBlank().isNotNull()
+        validate(LocationCreateParams::latitude).isNotNull()
+        validate(LocationCreateParams::longitude).isNotNull()
+    } ?: return
+
+    val target = transaction {
+        return@transaction LocationEntity.new {
+            name = params.name
+            pointX = params.latitude
+            pointY = params.longitude
+        }
+    }
+
+    call.respond(target.toRespondInfo())
+}
+
+@Route(Method.DELETE, "/location/{id}/delete")
+suspend fun locationDelete(call: ApplicationCall, principal: Principal) {
+    val locationID = call.parameters["id"]
+        ?: return call.respond(HttpStatusCode.BadRequest)
+
+    transaction {
+        return@transaction LocationEntity
+            .findById(locationID.toInt())
+            ?.delete()
+    } ?: return call.respond(HttpStatusCode.NotFound)
+
+    call.respond(HttpStatusCode.OK)
 }
